1feff4870d1e4bab44c1766df8da09393591ac7e
Yu Shen <lukeshnno1@gmail.com>
2024-02-07 15:44:28 +0800

    search

---
+++ game-recommender/src/main/java/sg/edu/nus/iss/gamerecommender/controller/CommonRestController.java
@@ -0,0 +1,27 @@
+package sg.edu.nus.iss.gamerecommender.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import sg.edu.nus.iss.gamerecommender.service.GameService;
+import sg.edu.nus.iss.gamerecommender.service.UserService;
+
+@RestController
+@RequestMapping(value = "/api/common")
+public class CommonRestController {
+	
+	@Autowired
+	GameService gameService;
+	
+	@Autowired
+	UserService userService;
+	
+	@PostMapping(value = "/search") 
+	public ResponseEntity<?> search(@RequestBody String body) {
+		
+	}
+}
