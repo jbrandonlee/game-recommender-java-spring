5439b10b64958b40a8f2c5177b50d033aa890092
Yu Shen <lukeshnno1@gmail.com>
2024-02-07 23:09:44 +0800

    search method complete

--- game-recommender/src/main/java/sg/edu/nus/iss/gamerecommender/controller/CommonRestController.java
+++ game-recommender/src/main/java/sg/edu/nus/iss/gamerecommender/controller/CommonRestController.java
@@ -1,12 +1,20 @@
 package sg.edu.nus.iss.gamerecommender.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import sg.edu.nus.iss.gamerecommender.model.Game;
+import sg.edu.nus.iss.gamerecommender.model.User;
 import sg.edu.nus.iss.gamerecommender.service.GameService;
 import sg.edu.nus.iss.gamerecommender.service.UserService;
 
@@ -20,8 +28,30 @@ public class CommonRestController {
 	@Autowired
 	UserService userService;
 	
-//	@PostMapping(value = "/search") 
-//	public ResponseEntity<?> search(@RequestBody String body) {
-//		
-//	}
+	@PostMapping(value = "/search") 
+	public ResponseEntity<?> search(@RequestBody String body) {
+		try {
+			JsonObject searchData = JsonParser.parseString(body).getAsJsonObject();
+			String type = searchData.get("type").getAsString();
+			String searchQuery=searchData.get("query").getAsString();
+			
+			switch(type) {
+				case "Game":
+					List<Game> gameList;
+					gameList = gameService.searchGames(searchQuery);
+					return ResponseEntity.ok(gameList);
+				case "Developer":
+					List<User> developerList;
+					developerList = userService.searchDevelopers(searchQuery);
+					return ResponseEntity.ok(developerList);
+				case "User":
+					List<User> userList;
+					userList = userService.searchGamers(searchQuery);
+					return ResponseEntity.ok(userList);
+			}
+			return null;
+		} catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.EXPECTATION_FAILED);
+        }
+	}
 }
