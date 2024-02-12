384e806393d6e36df26104cf1d1cdaadef58c46a
Yu Shen <lukeshnno1@gmail.com>
2024-02-12 17:41:39 +0800

    act feed

--- game-recommender/src/main/java/sg/edu/nus/iss/gamerecommender/service/ActivityService.java
+++ game-recommender/src/main/java/sg/edu/nus/iss/gamerecommender/service/ActivityService.java
@@ -1,9 +1,12 @@
 package sg.edu.nus.iss.gamerecommender.service;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 
 import sg.edu.nus.iss.gamerecommender.model.Activity;
 
 public interface ActivityService {
 	public Page<Activity> findUserActivity(int userId, int pageNo, int pageSize);
+	public List<Activity> findUserActivity(int userId);
 }
